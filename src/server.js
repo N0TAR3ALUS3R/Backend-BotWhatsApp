1d311c143a3023d7e3b2702d8dc228b17fd825b4
N0TAR3ALUS3R <iamsebas2403@outlook.com>
2025-08-04 11:16:02 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Backend WhatsApp bot corriendo en http://localhost:${PORT}`);
   console.log(`📘 Swagger UI: http://localhost:${PORT}/api-docs`);
 });
