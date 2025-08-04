1e0445af94846176dd0edbc2e2f00074ca9b7d09
N0TAR3ALUS3R <iamsebas2403@outlook.com>
2025-08-04 11:36:23 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Backend WhatsApp bot corriendo en http://localhost:${PORT}`);
-  console.log(`📘 Swagger UI: http://localhost:${PORT}/api-docs`);
+  console.log(`🚀 Backend WhatsApp bot corriendo en http://0.0.0.0:${PORT}`);
+  console.log(`📘 Swagger UI: http://0.0.0.0:${PORT}/api-docs`);
+  console.log(`🌐 Acceso externo: https://backend-botwhatsapp.fly.dev/:${PORT}/api-docs`);
 });
